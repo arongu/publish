aa156209ef6bf9f885543648c540a805f2ad13dc
Aron Gulyas <aron.gulyas.hu@gmail.com>
2020-12-01 21:49:09 +0100

    calculator snapshot

---
+++ calculator/js/view.js
@@ -0,0 +1,27 @@
+// display
+const display         = document.getElementById('calc-display-val')
+
+// controls
+const buttonClear     = document.getElementById('calc-clear');
+const buttonBackSpace = document.getElementById('calc-backspace');
+const buttonModulo    = document.getElementById('calc-modulo');
+const buttonDivide    = document.getElementById('calc-divide');
+const buttonMultiply  = document.getElementById('calc-multiply');
+const buttonSubtract  = document.getElementById('calc-subtract');
+const buttonAdd       = document.getElementById('calc-add');
+const buttonEquals    = document.getElementById('calc-equals');
+
+// numbers
+const buttonSeven   = document.getElementById('calc-seven');
+const buttonEight   = document.getElementById('calc-eight');
+const buttonNine    = document.getElementById('calc-nine');
+const buttonFour    = document.getElementById('calc-four');
+const buttonFive    = document.getElementById('calc-five');
+const buttonSix     = document.getElementById('calc-six');
+const buttonOne     = document.getElementById('calc-one');
+const buttonTwo     = document.getElementById('calc-two');
+const buttonThree   = document.getElementById('calc-three');
+const buttonZero    = document.getElementById('calc-zero');
+
+// decimal
+const buttonDecimal = document.getElementById('calc-decimal');
